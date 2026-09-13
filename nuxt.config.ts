697992c98655db9472e493897c9d202b6a5b7844
mgldvd <mgldvd@protonmail.com>
2026-09-13 18:44:52 -0500

    refactor(nuxt.config): improve formatting and add Plausible analytics script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,93 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const devPort = Number(import.meta.env.NUXT_PORT || import.meta.env.PORT || 3000)
+
+const devPort = Number(
+  import.meta.env.NUXT_PORT ||
+  import.meta.env.PORT ||
+  3000
+)
+
 const enableDevtools = import.meta.env.NUXT_DEVTOOLS === 'true'
 
 export default defineNuxtConfig({
   srcDir: 'app/',
+
   devServer: {
     host: 'localhost',
     port: devPort,
   },
+
   app: {
     baseURL: '/',
+
     head: {
       charset: 'utf-8',
+
       viewport: 'width=device-width, initial-scale=1',
+
       title: 'Gogh - Color Schemes',
+
       meta: [
-        { 'http-equiv': 'X-UA-Compatible', content: 'IE=edge' },
-        { name: 'description', content: 'Color Schemes for Terminals' },
-        { name: 'author', content: 'Miguel D. Quintero' },
+        {
+          'http-equiv': 'X-UA-Compatible',
+          content: 'IE=edge',
+        },
+        {
+          name: 'description',
+          content: 'Color Schemes for Terminals',
+        },
+        {
+          name: 'author',
+          content: 'Miguel D. Quintero',
+        },
       ],
+
       link: [
-        { rel: 'author', href: 'humans.txt' },
-        { rel: 'icon', type: 'image/png', sizes: '16x16', href: 'favicons/favicon.png' },
-        { rel: 'icon', href: 'favicons/favicon.ico' },
+        {
+          rel: 'author',
+          href: 'humans.txt',
+        },
+        {
+          rel: 'icon',
+          type: 'image/png',
+          sizes: '16x16',
+          href: 'favicons/favicon.png',
+        },
+        {
+          rel: 'icon',
+          href: 'favicons/favicon.ico',
+        },
+      ],
+
+      script: [
+        {
+          src: 'https://plausible.arcano.site/js/pa-Fw7nMNFtqNmPh7jU-bDaj.js',
+          async: true,
+        },
+
+        {
+          innerHTML: `
+            window.plausible = window.plausible || function () {
+              (plausible.q = plausible.q || []).push(arguments)
+            }
+
+            plausible.init = plausible.init || function (i) {
+              plausible.o = i || {}
+            }
+
+            plausible.init()
+          `,
+        },
       ],
     },
   },
+
   vite: {
     build: {
       modulePreload: {
         polyfill: false,
       },
     },
+
     optimizeDeps: {
       include: [
         'chroma-js',
@@ -40,6 +96,7 @@ export default defineNuxtConfig({
         'prismjs/components/prism-bash',
       ],
     },
+
     server: {
       hmr: {
         host: 'localhost',
@@ -47,20 +104,27 @@ export default defineNuxtConfig({
         protocol: 'ws',
       },
     },
+
     css: {
       preprocessorOptions: {
         scss: {
-          silenceDeprecations: ["legacy-js-api"]
-        }
-      }
-    }
+          silenceDeprecations: ['legacy-js-api'],
+        },
+      },
+    },
   },
+
   modules: [
     'nuxt-color-picker',
   ],
+
   plugins: [
     '~/assets/static/prism.client',
   ],
-  devtools: { enabled: enableDevtools },
-  compatibilityDate: '2024-04-03'
+
+  devtools: {
+    enabled: enableDevtools,
+  },
+
+  compatibilityDate: '2024-04-03',
 })
